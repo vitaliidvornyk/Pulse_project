35089a83b097772a4e8aa4fe56df6cbea2eb5489
vitaliidvornyk <59489402+vitaliidvornyk@users.noreply.github.com>
2020-03-01 11:53:16 +0200

    Add files via upload

--- mailer/smart.php
+++ mailer/smart.php
@@ -10,16 +10,16 @@ $mail->CharSet = 'utf-8';
 
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-$mail->isSMTP();                                         // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';                           // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                                   // Enable SMTP authentication
-$mail->Username = 'dvornikvitaliy@gmail.com';             // Наш логин
-$mail->Password = 'JackLONDON';                           // Наш пароль от ящика
-$mail->SMTPSecure = 'ssl';                                // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465;                                        // TCP port to connect to
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('dvornikvitaliy@gmail.com', 'Pulse');    // От кого письмо 
-$mail->addAddress('');             // Add a recipient
+$mail->setFrom('', 'Pulse');   // От кого письмо 
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
